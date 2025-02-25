471d83d760ab975bc1855304b88d531e01d1ad3a
hanc.han <chihchun.han@gmail.com>
2025-02-25 10:52:09 +0800

    fix: get /api/accb

--- api/accb.js
+++ api/accb.js
@@ -4,10 +4,15 @@ const app = express();
 
 // app.listen(3000, () => console.log("Server is running on port 3000"));
 
+const token = "1234";
+
+const shortcutAuth = (req) => req?.headers?.["h-auth"] === token;
+
 app.get("/api/accb", async (req, res) => {
-  res.status(200).json({
-    message: "Hello from accb!",
-  });
+  if (shortcutAuth(req)) {
+    res.status(200).send("Hello World");
+  }
+  res.status(401).send("Unauthorized");
 });
 
 export default app;
